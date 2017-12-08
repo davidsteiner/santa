759db1cd52d7a63391df6ea4992bd26fae87d3bd
David Steiner <david_j_steiner@yahoo.co.nz>
2017-12-08 20:02:45 +0000

    Add basic hspec testing

--- src/Santa.hs
+++ src/Santa.hs
@@ -1,28 +1,30 @@
-module Santa (findSolution) where
+module Santa (calculateKidID, findSolution) where
 
 import Data.Tuple
 
 
+-- Find the first kid's ID in any given sack
 findFirst :: Integer -> Integer
 findFirst 1    = 1
 findFirst sack = sack ^ 2 `quot` 2
 
-
 -- Calculate the kid's ID for given sack and present pair
-findKid :: (Integer, Integer) -> Integer
-findKid (sack, present) =
+calculateKidID :: (Integer, Integer) -> Integer
+calculateKidID (sack, present) =
   let
       initialSquare = if odd sack && sack > 1 then sack - 1 else sack
       finalSquare = initialSquare + present - 1
       sign = if odd $ initialSquare + finalSquare then 1 else -1
       summedSquares = sumTo finalSquare + sign * sumTo initialSquare
+  -- From the sum of squares we need to either subtract or add the first kid
+  -- depending on whether the present is odd or even
   in summedSquares - (1 - 2 * (present `rem` 2)) * findFirst sack
   where
     -- Sum numbers from 1 to n, e.g. for n = 3, then return 1 + 2 + 3 = 6
     sumTo n = (1 + n) * n `quot` 2
 
-findFactors :: Integer -> [(Integer, Integer)]
-findFactors n =
+calculateFactors :: Integer -> [(Integer, Integer)]
+calculateFactors n =
   -- Add the opposite pair of factors, e.g. (1, 10) => (10, 1)
   factorPairs ++ map swap (filter (uncurry (/=)) factorPairs)
   where
@@ -32,5 +34,5 @@ findFactors n =
     factorPairs = (\sack -> (sack, n `quot` sack)) <$> divisors
 
 findSolution :: Integer -> Integer
-findSolution x =
-  sum (findKid <$> findFactors x) `rem` 10^8
+findSolution n =
+  sum (calculateKidID <$> calculateFactors n) `rem` 10^8
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,25 @@
+import Santa
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Santa" $ do
+    describe "calculateKidID" $ do
+      it "returns 1 for first present in first sack" $ do
+        calculateKidID (1, 1) `shouldBe` (1 :: Integer)
+
+      it "returns 2 for first present in second sack" $ do
+        calculateKidID (2, 1) `shouldBe` (2 :: Integer)
+
+      it "returns 3 for second present in first sack" $ do
+        calculateKidID (1, 2) `shouldBe` (3 :: Integer)
+
+      it "returns 4 for first present in third sack" $ do
+        calculateKidID (3, 1) `shouldBe` (4 :: Integer)
+
+    describe "findSolution" $ do
+      it "returns 14257 for input 100" $ do
+        findSolution 100 `shouldBe` (14257 :: Integer)
+
+      it "returns 77301763 for input 6300000000" $ do
+        findSolution 6300000000 `shouldBe` (77301763 :: Integer)
